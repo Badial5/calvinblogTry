771069bc9d23e06ac32638964dec0fc03bd93fb9
Badial5 <omarbadialstudent@gmail.com>
2022-10-31 05:49:36 +0000

    Lesson6 URL parameters with React router added

---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+
+//pages
+import articles from "../Data/article/article-content"
+
+const ArticlePage = () => {
+
+    const {articleId} = useParams();
+
+    //using js find method
+    const article = articles.find(articleItem => articleItem.name === articleId)
+
+
+    return (
+        <>
+       <h1>{article.title}</h1>
+       <p>
+        {
+            article.content.map(content => (
+                <p>{content}</p>
+            ))
+        }
+       </p>
+
+       </>
+    );
+}
+
+export default ArticlePage;
